69cabe1c8ab7ff47e9a77111500dfe4f21f6264b
concept <conceptcodes@gmail.com>
2023-09-24 07:45:23 -0400

    add filtered events logic to event log

--- src/components/EventLog.tsx
+++ src/components/EventLog.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/select";
 
 import { useEvents } from "@/hooks/useEvents";
+import { useState } from "react";
 
 const Loading = () => {
   return (
@@ -23,14 +24,21 @@ const Loading = () => {
 
 const EventLog = () => {
   const { events } = useEvents();
+  const [filter, setFilter] = useState<string>("");
+
+  const filteredEvents = () => {
+    if (!events) return [];
+    if (filter === "") return events;
+    return events.filter((event) => event.type === filter);
+  };
 
   return (
     <div className="flex flex-col w-full xl:w-1/2 space-y-5">
       <div className="flex flex-row justify-between">
         <h1 className="text-4xl text-left font-bold">Events</h1>
-        <Select>
-          <SelectTrigger className="w-[150px]">
-            <SelectValue>Filter</SelectValue>
+        <Select onValueChange={setFilter} value={filter}>
+          <SelectTrigger className="max-w-[250px]">
+            <SelectValue placeholder="Filter"></SelectValue>
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
@@ -38,7 +46,7 @@ const EventLog = () => {
               <SelectItem value="MessageUnlocked">Message Unlocked</SelectItem>
               <SelectItem value="FeeUpdated">Fee Updated</SelectItem>
               <SelectItem value="MinimumLockUpTimeUpdated">
-                Minimum Lock Up Time Updated
+                Min LockUp Time Updated
               </SelectItem>
             </SelectGroup>
           </SelectContent>
@@ -46,7 +54,7 @@ const EventLog = () => {
       </div>
       <ScrollArea className="h-[500px] flex flex-col w-full space-y-5">
         {events ? (
-          events.map((event, i) => <Event key={i} event={event} />)
+          filteredEvents().map((event, i) => <Event key={i} event={event} />)
         ) : (
           <Loading />
         )}
